f30e3377d82407331fd39a3101ed78087af64247
kmlin <minglin@bigobject.io>
2020-09-25 18:24:24 +0800

    change origin to lemur0928/stailib; remove everything except cbe subdir

--- tt/space/cctv/cctv.go
+++
@@ -1,8 +0,0 @@
-package cctv
-
-// CCTV cctv
-type CCTV interface {
-	DetectRange(maxDetectDistance, degreeScale float64) Detect
-	ImagePixelToWorldPoint(u, v, height float64) Point
-	WorldPointToImagePixel(lng, lat, height float64) Pixel
-}
